e9c7d6d9348a37533a41f8219b2a5feedb91bc22
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-04-18 16:28:15 +0700

    menambahkan API mahasiswa

---
+++ jurnal9_1302213043/jurnal9_1302213043/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jurnal9_1302213043.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class mahasiswa : Controller
+    {
+        public static List<Mahasiswa> mhs = new List<Mahasiswa>()
+        {
+            new Mahasiswa {Name = "Jaatsiyah Maulidina Astrianto", Nim = "1302213043",Course = new List<string>{"PBO","KPL","BASDAT","SQA","JARKOM","TPLM","UX","PROTING2" }, Year = 2021 },
+            new Mahasiswa {Name = "Hilma Fariz Hirzi", Nim = "1302213092",Course = new List<string>{"PBO","KPL","BASDAT","SQA","JARKOM","TPLM","UX","PROTING2" }, Year = 2021 },
+            new Mahasiswa {Name = "Putri Auliya Rahmah", Nim = "1302210033",Course = new List<string>{"PBO","KPL","BASDAT","SQA","JARKOM","TPLM","UX","PROTING2" }, Year = 2021 }
+        };
+
+        // GET/api/Mahasiswa : Mengembalikan output berupa list/array dari semua objek Mahasiswa
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mhs;
+        }
+
+        // GET/api/Mahasiswa/{id} : Mengembalikan output berupa objek mahasiswa untuk index "id"
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mhs[id];
+        }
+
+        // POST api/Mahasiswa : Menambahkan objek Mahasiswa baru
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa newMahasiswa)
+        {
+            mhs.Add(newMahasiswa);
+            return Ok();
+        }
+
+        // DELETE api/Mahasiswa/{id} : menghapus objek mahasiswa pada index "id"
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id >= mhs.Count)
+            {
+                return NotFound();
+            }
+
+            mhs.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
---
+++ jurnal9_1302213043/jurnal9_1302213043/Mahasiswa.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal9_1302213043
+{
+    public class Mahasiswa
+    {
+        public string Name { get; set; }
+        public string Nim { get; set; }
+        public List<string> Course { get; set;}
+        public int Year { get; set; }
+    }
+}
